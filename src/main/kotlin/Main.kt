584e581e7cab2ad85b463d2feb30d68f89536f5e
Mendez <juan.mendez1@grainger.com>
2022-02-07 21:31:58 -0600

    00.02: suspend along with jobs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,23 @@ import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 const val NUMTASKS = 10_000
 const val LOOPS = 500
 const val WAITMS = 10L
 
 // runBlocking is also a coroutine builder that bridges the non-coroutine world of a regular fun main()
-fun usingCoroutines() = runBlocking {
+// it is also a coroutine scope, which is a wrapper of child coroutines. in this way it completes when all of them complete.
+
+// lets now use another type of coroutine scope which by the way is suspended, so it doesn't put on hold other code execution
+// like runBlocking does, but it runs asynchronously.
+// suspend is a qualifier that describes the function as asynchronous.
+// Dart for example uses `void usingCoroutines async() {}`
+suspend fun usingCoroutines() = coroutineScope {
     val result = AtomicInteger()
 
     // Job is a lifecycle observer/controller of a coroutine
@@ -35,7 +42,32 @@ fun usingCoroutines() = runBlocking {
     }
 }
 
-fun usingThreads(args: Array<String>) {
+// lets do a simple example
+suspend fun sayHelloWorldAsync() = coroutineScope {
+    var helloWorld = "!"
+
+    val job1 = launch {
+        delay(2000L)
+        helloWorld += " world"
+        println(helloWorld)
+    }
+
+    val job2 = launch {
+        delay(1000L)
+        helloWorld += "hello"
+        println(helloWorld)
+    }
+
+    // both coroutines run concurrently
+    println(helloWorld)
+
+    // lets join all jobs and wait
+    joinAll(job1, job2).apply {
+        println("completed: $helloWorld")
+    }
+}
+
+fun usingThreads() {
     val result = AtomicInteger()
     val threads = mutableListOf<Thread>()
 
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test
 class MainTest {
     @Test
     fun `this is just a test1`() = runTest {
+        // since method is asynchronous it has to be access through another scope.
+        // just like what runBlocking did in the previous branch
         usingCoroutines()
+    }
+
+    @Test
+    fun `say hello world asyncronosly`() = runTest {
+        sayHelloWorldAsync()
     }
 }
