436603e9cd8151826ab35d70bb44a5b88e612cfa
Mendez <juan.mendez1@grainger.com>
2022-02-08 19:24:08 -0600

    00.03: how coroutines can be cancelled

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,73 @@
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 
-fun main(args: Array<String>) = runBlocking {
-    println("Hello World!")
+suspend fun holdCoroutineUntilCompletes() = coroutineScope {
+    val job = launch {
+        delay(1000)
+        println("hello")
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Suspends the coroutine until this job is complete
+    job.join()
+    println("world")
+
+    // without join()... world and then hello
+}
+
+suspend fun cancelCoroutine(cancelAtSeconds: Long? = null) = coroutineScope {
+    val job = launch {
+        repeat(1000) {
+            delay(10)
+            print(".")
+        }
+    }
+
+    // wait until it completes
+    cancelAtSeconds?.let { seconds ->
+        delay(seconds)
+        job.cancel()
+    } ?: run {
+        job.join()
+    }
+
+
+    println("done")
+}
+
+suspend fun timeOutCoroutine(cancelAtSeconds: Long? = null) = coroutineScope {
+    // wait until it completes
+    cancelAtSeconds?.let { seconds ->
+        try {
+            withTimeout(seconds) {
+                repeat(1000) {
+                    delay(10)
+                    print(".")
+                }
+            }
+        } catch (e: TimeoutCancellationException) {
+            println("\n$e")
+        }
+
+    } ?: run {
+        launch {
+            repeat(1000) {
+                delay(10)
+                print(".")
+            }
+        }.join()
+    }
+
+
+    println("done")
+}
+
+suspend fun timeOutWithNoException(cancelAtSeconds: Long) = withTimeoutOrNull(cancelAtSeconds) {
+    repeat(1000) {
+        delay(10)
+        print(".")
+    }
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,9 +1,29 @@
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class MainTest {
     @Test
-    fun `this is just a test1`() {
-        assertEquals(1, 1)
+    fun `this is just a test1`() = runTest {
+        holdCoroutineUntilCompletes()
+    }
+
+    @Test
+    fun `coroutines with cancellation`() = runTest {
+        // we can use timeouts as well.. :)
+        cancelCoroutine(250)
+    }
+
+    @Test
+    fun `coroutines with time-out`() = runTest {
+        timeOutCoroutine(300)
+    }
+
+    @Test
+    fun `coroutines time-out with no exception`() = runTest {
+        val job = timeOutWithNoException(300)
+
+        if (job == null) {
+            println("done")
+        }
     }
 }
